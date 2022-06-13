ece194765c2ed0bc9f88dd46c4a3345d90690727
gubik <gubaxxxl@gmail.com>
2022-06-13 14:38:58 +0200

    Remove obsolete code

--- src/Service/Api/ApiClient.php
+++ src/Service/Api/ApiClient.php
@@ -5,9 +5,7 @@ namespace App\Service\Api;
 use App\Utils\Api\RequestBlueprint;
 use App\Utils\Api\ResourceData;
 use RuntimeException;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Contracts\HttpClient\ResponseInterface;
@@ -69,123 +67,5 @@ class ApiClient
         }
 
         return $responses;
-    }
-
-    /**
-     * Vytvori asynchronni request dle vzoru.
-     *
-     * @param RequestBlueprint $blueprint
-     * @return PromiseInterface
-     */
-    private function createPromise(RequestBlueprint $blueprint): PromiseInterface
-    {
-        // vytvori objekt Guzzle requestu
-        $request = $this->createRequest($blueprint);
-
-        // callback v pripade, ze asynchronni request probehl v poradku
-        $onFulfilled = function (Response $response) use ($blueprint) {
-            $json = json_decode((string)$response->getBody(), true);
-            if ($json === null) {
-                throw new BadResponseException("Chybna data z API pro URL {$blueprint->getUri()}.");
-            }
-
-            $this->data->{$blueprint->getContainerName()} = $json;
-
-            if ($blueprint->isCacheAvailable()) {
-                $elapsedTime = microtime(true) - $blueprint->getRequestStartTimestamp();
-                $this->cacheHelper->saveCacheItem($blueprint->getCacheKey(), $json, $blueprint->getCacheLifetime(), $elapsedTime);
-            }
-        };
-
-        return $this->client->sendAsync($request)->then($onFulfilled);
-    }
-
-    /**
-     * Vytvori request dle vzoru pro komunikaci s API.
-     *
-     * @param RequestBlueprint $blueprint
-     * @return Request
-     */
-    private function createRequest(RequestBlueprint $blueprint): Request
-    {
-        $headers = $this->getCommonHeaders();
-
-        if ($blueprint->getBodyData()) {
-            $headers += [
-                "Content-Type" => "application/json"
-            ];
-
-            return new Request(
-                $blueprint->getMethod(),
-                $blueprint->getUri(),
-                $headers,
-                json_encode($blueprint->getBodyData())
-            );
-        }
-
-        $blueprint->setRequestStartTimestamp(microtime(true));
-
-        return new Request($blueprint->getMethod(), $blueprint->getUri(), $headers);
-    }
-
-    private function getCommonHeaders(): array
-    {
-        $headers = [];
-
-        if ($this->request) {
-            $headers["X-User-Ip"] = $this->request->getClientIp();
-            $headers["X-Request-Uri"] = $this->request->getSchemeAndHttpHost() . $this->request->getRequestUri();
-            $headers["X-User-Agent"] = $this->request->headers->get("User-Agent");
-        }
-
-        $headers["X-Api-Key"] = $this->apiKey;
-        $headers["X-App-Domain"] = self::X_APP_DOMAIN_HEADER;
-
-        return $headers;
-    }
-
-    /**
-     * Odesle asnychronni pozadavky reprezentovane polem objektu PromiseInterface.
-     *
-     * @param PromiseInterface[] $promises
-     */
-    private function send(array $promises): void
-    {
-        (new EachPromise($promises))->promise()->wait();
-    }
-
-    /**
-     * Vrati vsechny rejected promises (neuspesne asynchronni reuqesty).
-     *
-     * @return PromiseInterface[]
-     */
-    private function getRejectedPromises(): array
-    {
-        $rejected = [];
-        foreach ($this->promises as $promise) {
-            if ($promise->getState() === PromiseInterface::REJECTED) {
-                $this->checkStatusCode($promise);
-                $rejected[] = $promise;
-            }
-        }
-        return $rejected;
-    }
-
-    /**
-     * Zkontroluje HTTP stavovy kod odpovedi a prislusne na nej pripadne zareaguje.
-     *
-     * @param PromiseInterface $promise
-     */
-    private function checkStatusCode(PromiseInterface $promise): void
-    {
-        /** @var \Throwable $throwable */
-        $throwable = inspect($promise)["reason"];
-
-        $statusCode = $throwable->getCode();
-
-        if ($statusCode === SymfonyResponse::HTTP_NOT_FOUND) {
-            // Pozadovany zdroj nebyl nalezen
-            throw new NotFoundHttpException($throwable->getMessage());
-        }
     }
 }
